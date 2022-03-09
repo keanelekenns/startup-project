6b3b149451656c21cbed1e1151a65415860d7b0e
Keanelek Enns <43152879+keanelekenns@users.noreply.github.com>
2022-03-08 22:47:53 -0800

    DevXP: Initialized Terraform

---
+++ terraform.tf
@@ -0,0 +1,117 @@
+terraform {
+  required_providers {
+    aws =  {
+    source = "hashicorp/aws"
+    version = ">= 2.7.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_backend_bucket" {
+      bucket = "terraform-state-ij80jw2ztsoy6cq1jd5b2cn650o6e3hbx4frluwj5hsrz"
+}
+
+resource "aws_instance" "asdf" {
+      ami = data.aws_ami.amazon_latest.id
+      instance_type = "t2.micro"
+      lifecycle {
+        ignore_changes = [ami]
+      }
+      subnet_id = aws_subnet.devxp_vpc_subnet_public0.id
+      associate_public_ip_address = true
+      vpc_security_group_ids = [aws_security_group.devxp_security_group.id]
+      iam_instance_profile = aws_iam_instance_profile.asdf_iam_role_instance_profile.name
+}
+
+resource "aws_eip" "asdf_eip" {
+      instance = aws_instance.asdf.id
+      vpc = true
+}
+
+resource "aws_iam_instance_profile" "asdf_iam_role_instance_profile" {
+      name = "asdf_iam_role_instance_profile"
+      role = aws_iam_role.asdf_iam_role.name
+}
+
+resource "aws_iam_role" "asdf_iam_role" {
+      name = "asdf_iam_role"
+      assume_role_policy = "{\n  \"Version\": \"2012-10-17\",\n  \"Statement\": [\n    {\n      \"Action\": \"sts:AssumeRole\",\n      \"Principal\": {\n        \"Service\": \"ec2.amazonaws.com\"\n      },\n      \"Effect\": \"Allow\",\n      \"Sid\": \"\"\n    }\n  ]\n}"
+}
+
+resource "aws_subnet" "devxp_vpc_subnet_public0" {
+      vpc_id = aws_vpc.devxp_vpc.id
+      cidr_block = "10.0.0.0/25"
+      map_public_ip_on_launch = true
+      availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "devxp_vpc_subnet_public1" {
+      vpc_id = aws_vpc.devxp_vpc.id
+      cidr_block = "10.0.128.0/25"
+      map_public_ip_on_launch = true
+      availability_zone = "us-west-2b"
+}
+
+resource "aws_internet_gateway" "devxp_vpc_internetgateway" {
+      vpc_id = aws_vpc.devxp_vpc.id
+}
+
+resource "aws_route_table" "devxp_vpc_routetable_pub" {
+      route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.devxp_vpc_internetgateway.id
+      }
+      vpc_id = aws_vpc.devxp_vpc.id
+}
+
+resource "aws_route" "devxp_vpc_internet_route" {
+      route_table_id = aws_route_table.devxp_vpc_routetable_pub.id
+      destination_cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.devxp_vpc_internetgateway.id
+}
+
+resource "aws_route_table_association" "devxp_vpc_subnet_public_assoc" {
+      subnet_id = aws_subnet.devxp_vpc_subnet_public0.id
+      route_table_id = aws_route_table.devxp_vpc_routetable_pub.id
+}
+
+resource "aws_vpc" "devxp_vpc" {
+      cidr_block = "10.0.0.0/16"
+      enable_dns_support = true
+      enable_dns_hostnames = true
+}
+
+resource "aws_security_group" "devxp_security_group" {
+      vpc_id = aws_vpc.devxp_vpc.id
+      name = "devxp_security_group"
+      ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+      egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+}
+
+data "aws_ami" "amazon_latest" {
+      most_recent = true
+      owners = ["585441382316"]
+      filter {
+        name = "name"
+        values = ["*AmazonLinux*"]
+      }
+      filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+      }
+}
+
